a12c4a840f4608f8f299cc45f4c559308db939c8
evcastelani <emersonvitor@gmail.com>
2017-11-28 18:21:10 -0200

    Now is not require the objects.jl

--- src/Liga.jl
+++ src/Liga.jl
@@ -1,14 +1,14 @@
-include("layout.jl")
+
+
 module Liga
-import Base.show, Base.copy, Base.subtypes
+import Base.show
+import Base.copy
+import Base.subtypes
 importall Base.Operators
 #########################################################
-export
-#TYPES
-kbasis, kmultvec, kblade,
+export kbasis, kmultvec, kblade,
 pbasis, pmultvec, pblade,
 cbasis, cmultvec, cblade,
-#FUNCTIONS
 kb, grade, mvectovec, copy,
 geoprod, inner, outer, scalar,
 bscalar, mvsum, mvreverse, dual,
@@ -21,7 +21,71 @@ retoaffin, affine, iretoaffin,
 euctoga, S, Hm, H, ovector, iH, irH,
 pconformal, ipconformal, conformal,
 iconformal, cbltopbl, pbltocbl
+
+
 #########################################################
+
+#layout
+
+function arvore(v,niv,pos,lista,n)
+    if niv<=n 
+        if pos==0
+            aux=copy(v)
+            aux[niv]=true 
+            niv=niv+1
+            lista=push!(lista,aux)
+            lista=arvore(aux,niv,0,lista,n) 
+            lista=arvore(aux,niv,1,lista,n) 
+            
+        else
+            niv=niv+1
+            aux=copy(v)
+            lista=arvore(aux,niv,0,lista,n)
+            lista=arvore(aux,niv,1,lista,n)
+            
+        end    
+    end
+    return lista
+end
+
+function buildtree(n)
+   v=falses(n)
+   lista=[v]
+   for i in arvore(v,1,0,[v],n)[2:length(arvore(v,1,0,[v],n))]
+        lista=push!(lista,i)
+   end     
+   for i in arvore(v,1,1,[v],n)[2:length(arvore(v,1,1,[v],n))]
+        lista=push!(lista,i)
+   end
+   return lista
+end
+""" This function is used to define the space to work 
+## Example
+```julia-repl
+julia> layout(3)
+```
+generates a G3 space with base 1,e1,e2,e3,e12,e13,e23,e123
+
+"""
+function layout(dim::Int)
+bn=buildtree(dim)
+for v in bn
+    s=find(x->x==true,v)
+    if isempty(s)
+        eval(parse("const id = $v"))
+    else     
+        conc=string(s[1])
+        for k=2:length(s)
+            conc=string(conc,s[k])
+        end
+        eval(parse("const e$(conc) = $v"))
+    end
+end
+end
+
+
+
+########################################################
 """
 ```
 c(x::Vector{Bool}, a::Number)
@@ -3325,3 +3389,65 @@ function mvreverse(a::cmultvec)
 end
 #########################################################
 end
+
+
+
+
+function arvore(v,niv,pos,lista,n)
+    if niv<=n 
+        if pos==0
+            aux=copy(v)
+            aux[niv]=true 
+            niv=niv+1
+            lista=push!(lista,aux)
+            lista=arvore(aux,niv,0,lista,n) 
+            lista=arvore(aux,niv,1,lista,n) 
+            
+        else
+            niv=niv+1
+            aux=copy(v)
+            lista=arvore(aux,niv,0,lista,n)
+            lista=arvore(aux,niv,1,lista,n)
+            
+        end    
+    end
+    return lista
+end
+
+function buildtree(n)
+   v=falses(n)
+   lista=[v]
+   for i in arvore(v,1,0,[v],n)[2:length(arvore(v,1,0,[v],n))]
+        lista=push!(lista,i)
+   end     
+   for i in arvore(v,1,1,[v],n)[2:length(arvore(v,1,1,[v],n))]
+        lista=push!(lista,i)
+   end
+   return lista
+end
+""" This function is used to define the space to work 
+## Example
+```julia-repl
+julia> layout(3)
+```
+generates a G3 space with base 1,e1,e2,e3,e12,e13,e23,e123
+
+"""
+function layout(dim::Int)
+bn=buildtree(dim)
+for v in bn
+    s=find(x->x==true,v)
+    if isempty(s)
+        eval(parse("const id = $v"))
+    else     
+        conc=string(s[1])
+        for k=2:length(s)
+            conc=string(conc,s[k])
+        end
+        eval(parse("const e$(conc) = $v"))
+    end
+end
+Liga.layout(dim)
+end
+
+
